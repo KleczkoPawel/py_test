7633c9240f79de286cddbab0c98ec2233d57c43d
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-19 11:04:30 +0200

    add firefox binary click.py

--- click.py
+++ click.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-driver = webdriver.Firefox()
+firefox_binary = FirefoxBinary()
+driver = webdriver.Firefox(firefox_binary=firefox_binary)
 driver.get("https://www.cvedetails.com/vulnerability-list/vendor_id-12752/product_id-25450/Mongodb-Mongodb.html")
 element = driver.find_element(By.XPATH, '//button[text()="Download Results"]')
 element.click()
