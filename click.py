7f24e194a1a906c67227ed178e5830d6e09809c8
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-19 11:43:58 +0200

    add correct imports click.py

--- click.py
+++ click.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import FirefoxOptions
+
 
 #options = Options()
 #options.binary_location = r"/home/firefox/firefox-bin"
