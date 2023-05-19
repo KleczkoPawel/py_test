a13f8991e54610b1e76e6bc2bcd8be8d67f658b8
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-19 13:30:04 +0200

    add try profile click.py

--- click.py
+++ click.py
@@ -3,17 +3,22 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import FirefoxOptions
 import time
-
+from selenium.webdriver.firefox.options import Options
 
 #options = Options()
 #options.binary_location = r"/home/firefox/firefox-bin"
-#
+
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 
-profile = webdriver.FirefoxProfile()
+""" profile = webdriver.FirefoxProfile()
 default_download_path = profile['browser.download.dir']
-print(default_download_path)
+print(default_download_path) """
+
+profile = webdriver.FirefoxProfile(firefox_profile=profile)
+profile.set_preference("browser.download.dir", "/nowa/lokalizacja")
+profile.set_preference("browser.download.folderList", 2)
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
 
 driver = webdriver.Firefox(options=opts, firefox_binary="/home/firefox/firefox-bin")
 driver.get("https://www.cvedetails.com/vulnerability-list/vendor_id-12752/product_id-25450/Mongodb-Mongodb.html")
