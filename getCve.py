8ef4c29bd9ca5c7d964c742e50996338a0d05d48
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-22 13:45:38 +0200

    connect to opencve api

--- getCve.py
+++ getCve.py
@@ -9,8 +9,3 @@ password = 'Lexmarkz12'
 response = requests.get(apiUrl, auth = HTTPBasicAuth(username, password))
 
 print(response.json())
-
-CVE-2017-15535
-CVE-2020-7610
-VE-2022-48282
-CVE-2020-7923"
