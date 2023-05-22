f300a97ba37018711115d0a033f40bae0f487c4a
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-22 13:45:26 +0200

    connect to opencve api

--- getCve.py
+++ getCve.py
@@ -1,14 +1,16 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-apiUrl = "https://www.opencve.io/api/cve"
-params = {
-    "product": "mongodb",
-    "cvss": "critical,high"
-}
+apiUrl = "https://www.opencve.io/api/cve?product=mongodb&cvss=critical"
+
 username = 'pawel'
 password = 'Lexmarkz12'
 
-response = requests.get(apiUrl, params=params, auth = HTTPBasicAuth(username, password))
+response = requests.get(apiUrl, auth = HTTPBasicAuth(username, password))
 
 print(response.json())
+
+CVE-2017-15535
+CVE-2020-7610
+VE-2022-48282
+CVE-2020-7923"
