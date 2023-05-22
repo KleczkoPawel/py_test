e776ede96db4e35af37a31e5aaeb97264a4d9c60
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-22 12:58:05 +0200

    connect to opencve api

--- getCve.py
+++ getCve.py
@@ -1,7 +1,15 @@
 import requests
-from requests.auth import HTTPDigestAuth
 
+apiUrl = "https://www.opencve.io/api/cve?product=mongodb&cvss=high"
+# response = requests.get(api_url, auth=HTTPDigestAuth('pawel', 'Lexmarkz12'))
 
-api_url = "https://www.opencve.io/api/cve?product=mongodb&cvss=high"
-response = requests.get(api_url, auth=HTTPDigestAuth('pawel', 'Lexmarkz12'))
-print(response)
+username = 'pawel'
+password = 'Lexmarkz12'
+
+session = requests.Session()
+response = session.post(
+    apiUrl,
+    json={'identifier': username, 'password': password},
+    headers={'VERSION': '2'},
+)
+response.raise_for_status()  # if not a 2xx response, raise an exception
