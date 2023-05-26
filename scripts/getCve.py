b7445716ac367c0205397a1d59a41290d68e4df4
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-26 13:14:50 +0200

    with function and higher version compare

--- scripts/getCve.py
+++ scripts/getCve.py
@@ -7,7 +7,7 @@ dbUrl = sys.argv[1]
 dbVersion = sys.argv[2]
 dbHigherVersion = dbVersion.split(".")
 dbHigherVersion[-1] = str(int(dbHigherVersion[-1]) + 1)
-dbHigherVersion = join(dbHigherVersion)
+dbHigherVersion = ".".join(dbHigherVersion)
 print(dbHigherVersion)
 #
 # def countDatabaseVulnerabilities(url, checkVersion):
