d38a3d5ac90d49cff7fde615d460d888b42d7c34
Paweł Kleczko <pawel.kleczko@cardinality.co.uk>
2023-05-26 13:13:06 +0200

    with function and higher version compare

--- scripts/getCve.py
+++ scripts/getCve.py
@@ -6,7 +6,7 @@ import sys
 dbUrl = sys.argv[1]
 dbVersion = sys.argv[2]
 dbHigherVersion = dbVersion.split(".")
-dbHigherVersion[-1] = int(dbHigherVersion[-1]) + 1
+dbHigherVersion[-1] = str(int(dbHigherVersion[-1]) + 1)
 # dbHigherVersion = ".".join(str(dbHigherVersion))
 print(dbHigherVersion)
 #
